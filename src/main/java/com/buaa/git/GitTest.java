3476b7d15cf545648f49bfb1a4d4fc4108102325
lxy <qq1291280106@126.com>
2025-09-07 21:26:52 +0800

    java第四次提交

--- src/main/java/com/buaa/git/GitTest.java
+++ src/main/java/com/buaa/git/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello buaa!");
+        System.out.println("hello thu!");
+        System.out.println("hello pku!");
     }
 
 }
